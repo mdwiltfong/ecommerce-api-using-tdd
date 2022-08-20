bcd38bcc9b5be7616b0c2634436adbe3bc10ad64
Cecil Mahumane <cecmahumane@gmail.com>
2022-08-19 22:13:40 -0400

    profile.js route added to server folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = 5000;
 
+//Middleware
+app.use(cors());
+app.use(express.json())
 
 app.listen(PORT, () => {
     console.log(`server has started on port ${PORT}`)
