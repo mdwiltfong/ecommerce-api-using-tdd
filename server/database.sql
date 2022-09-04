cc7ccdd9de9378fc0feb8400aba04dffda757945
Cecil Mahumane <cecmahumane@gmail.com>
2022-09-04 10:09:25 -0400

    Product table fully populated

--- server/database.sql
+++ server/database.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE ecommerceapi;
 
-CREATE TABLE [IF NOT EXISTS] user(
+CREATE TABLE IF NOT EXISTS user(
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(255), 
     passwordhash VARCHAR(255),
     order_id VARCHAR (255)    
 );
 
-CREATE TABLE [IF NOT EXISTS] products(
+CREATE TABLE IF NOT EXISTS products(
     id SERIAL PRIMARY KEY,
     product_name VARCHAR(255),
-    product_description VARCHAR(255),
+    product_description VARCHAR(1000),
     unit_price numeric,
     quantity_small numeric,
     quantity_med numeric,
@@ -19,10 +19,22 @@ CREATE TABLE [IF NOT EXISTS] products(
     image2 VARCHAR(255)
 );
 
+
+
 INSERT INTO products(product_name, unit_price, quantity_small, quantity_med, quantity_large, image1, image2)
 VALUES ('T-Shirt', 35.00, 25, 25, 25, '../src/images/Tshirt.jpg', '../src/images/Greytshirt.png');
-       ('Hoodie', 49.00, 25, 25, 25, '../src/images/Hoodie.jpg', '../src/images/Greyhoodie.png');   
+INSERT INTO products(product_name, unit_price, quantity_small, quantity_med, quantity_large, image1, image2)
+VALUES ('Hoodie', 49.00, 25, 25, 25, '../src/images/Hoodie.jpg', '../src/images/Greyhoodie.png');   
 
-INSERT INTO products(product_description)
-VALUES ('So classy it hurts', '100% combed ring-spun cotton', 'Printed on Next Level garment', 'Pre-shrunk', 'Tear-away label');
-       ('Comfy. Cozy. Collective Podcast', '100% California fleece cotton', 'Super sweet drawstrings', 'Raglan sleeves', 'Front pouch pocket');
+UPDATE products SET product_description ='<p data-test="p1">So classy it hurts.</p> 
+                                          <p data-test="p2">100% combed ring-spun cotton</p> 
+                                          <p data-test="p3">Printed on Next Level garment</p> 
+                                          <p data-test="p4">Pre-shrunk</p> 
+                                          <p data-test="p5">Tear-away label</p>'
+WHERE product_name = 'T-Shirt';
+UPDATE products SET product_description ='<p data-test="p1">Comfy. Cozy.</p> 
+                                          <p data-test="p2">100% California fleece cotton</p> 
+                                          <p data-test="p3">Super sweet drawstrings</p> 
+                                          <p data-test="p4">Raglan sleeves</p> 
+                                          <p data-test="p5">Front pouch pocket</p>'
+WHERE product_name = 'Hoodie';
