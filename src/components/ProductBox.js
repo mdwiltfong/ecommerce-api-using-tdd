58d9487b084a36e3917d68823f95107ffe811a59
Cecil Mahumane <cecmahumane@gmail.com>
2022-10-09 15:05:34 -0400

    Axios package installed

--- src/components/ProductBox.js
+++ src/components/ProductBox.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+
 const ProductBox = () => {
   const [allProductData, setAllProductData] = React.useState([]);
   const [tshirtImage, setTshirtImage] = React.useState([]);
