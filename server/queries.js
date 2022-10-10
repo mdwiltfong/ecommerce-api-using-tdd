4160ee48d8cc2c2c1e4a56a79f6b9a02dd50125c
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2022-10-10 11:57:08 -0400

    Update queries.js
    As I was shamelessly copying this, I was thinking that maybe using classes would be better. That way, you can use dot notation when accessing these queries. 
    
    So for example, at the moment you are using this `queries.js` module like this : 
    
    ```
       try {
            const checkCart = await pool.query(queries.checkExists, [productId]);
            if (checkCart.rows.length) {
    
            }
        } catch (err) {
            console.log(err.message)
        }
    ```
    
    `queries.checkExists` doesn't really say much. Someone looking at your code would have to explore what the query actually is. On top of that, if you were to begin using these queries in other places, you may begin to question what table is this query actually using. In which case you can waste time looking up the query again. 
    
    With this Object Orient Programming approach, you can use dot notation which can convey more information. 
    
    `queries.sessionQueries.checkSession`

--- server/queries.js
+++ server/queries.js
@@ -1,19 +1,39 @@
-const insertLogin = "INSERT INTO users(email, passwordhash) VALUES ($1, $2)";
-const checkEmailExists = "SELECT email FROM users WHERE users.email = $1";
-const getProductsData = "SELECT * FROM products";
-const productData = "SELECT * FROM products WHERE products.product_name = $1";
-const productId = "SELECT products.id FROM products WHERE products.product_name = $1";
-const checkExists = "SELECT * FROM user_products WHERE user_products.product_id = $1";
-const addOne = "UPDATE user_products SET quantity = ";
-const checkSession = "SELECT * FROM session";
+
+
+
+
+class usersQueries{
+    insertLogin = "INSERT INTO users(email, passwordhash) VALUES ($1, $2)";
+    checkEmailExists = "SELECT email FROM users WHERE users.email = $1";
+    
+}
+
+class productsQueries{
+    getProductsData = "SELECT * FROM products";
+    productData = "SELECT * FROM products WHERE products.product_name = $1";
+    productId = "SELECT products.id FROM products WHERE products.product_name = $1";
+}
+
+class usersProductsQueries{
+    checkExists = "SELECT * FROM user_products WHERE user_products.product_id = $1";
+    addOne = "UPDATE user_products SET quantity = $1";
+}
+
+
+class sessionQueries{
+    checkSession = "SELECT * FROM session";
+}
+
+const sessionQueries= new sessQueries();
+const usersProductsQueries = new usersProductsQueries();
+const usersQueries= new usersQueries();
+
+
+
+
 
 module.exports = {
-    insertLogin,
-    checkEmailExists,
-    getProductsData,
-    productData,
-    productId,
-    checkExists,
-    addOne,
-    checkSession
+   sessionQueries,
+    usersProductsQueries,
+    usersQueries
 }
