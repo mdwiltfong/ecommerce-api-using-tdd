e75ad863da619fbdf90f4b0534d91675e1767510
Cecil Mahumane <cecmahumane@gmail.com>
2022-10-26 19:01:12 -0400

    Cart sized images added to public folder

---
+++ src/components/ShoppingCart.cy.js
@@ -0,0 +1,10 @@
+import ShoppingCart from "./ShoppingCart";
+
+describe('Cart page', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080)
+        cy.mount(<ShoppingCart/>)
+    })
+    it('')
+})
+
